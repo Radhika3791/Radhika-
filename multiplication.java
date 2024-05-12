f3e59bee88daf8b64b8cafa0e87ac31cf4344ddf
Radhika3791 <167883544+Radhika3791@users.noreply.github.com>
2024-05-12 13:31:34 +0530

    Update

---
+++ multiplication.java
@@ -0,0 +1,11 @@
+public class Multiplication {
+    
+    public static void main(String[] args) {
+        int num1 = 5; // Example number 1
+        int num2 = 7; // Example number 2
+        
+        int product = num1 * num2; // Multiplying num1 and num2
+        
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
